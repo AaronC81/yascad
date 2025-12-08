5c47370e5e6a09ae73734ed41001b52eb8935b50
Aaron Christiansen <aaronc20000@gmail.com>
2025-12-08 21:15:00 +0000

    GUI: Fix crash when rendering with no resulting geometry

--- gui/src/components/RenderCanvas.tsx
+++ gui/src/components/RenderCanvas.tsx
@@ -4,10 +4,16 @@ import { STLLoader } from "three/examples/jsm/Addons.js";
 import { Suspense, useMemo } from "react";
 
 export default function RenderCanvas({ stl }: { stl: string | undefined }) {
+  // Three.js `STLLoader` throws an exception when an STL has no triangles.
+  //
+  // Guard against this with a completely rubbish heuristic, by checking for the text "facet normal"
+  // in the STL text, which appears for every triangle.
+  const stlHasTriangles = useMemo(() => stl?.includes("facet normal"), [stl])
+
   return (
     <Canvas>
       <ambientLight intensity={Math.PI / 3} />
-      {stl &&
+      {stl && stlHasTriangles &&
         <Suspense>
           <STL stl={stl} />
         </Suspense>
