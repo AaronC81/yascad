c45ad4353879a5fc7dfb891fc2f5342e54661d8b
Aaron Christiansen <aaronc20000@gmail.com>
2025-12-13 20:09:20 +0000

    GUI: Make UI panels resizable

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -6,6 +6,7 @@ import { writeTextFile } from "@tauri-apps/plugin-fs";
 import useKeyboardShortcut from "./hooks/useKeyboardShortcut";
 import { editor } from "monaco-editor";
 import ModelEditor from "./components/ModelEditor";
+import { PanelGroup, Panel, PanelResizeHandle } from "react-resizable-panels";
 
 function App() {
   const [lastStl, setLastStl] = useState("");
@@ -58,29 +59,33 @@ function App() {
   useKeyboardShortcut({ key: "F5" }, renderPreview, [renderPreview]);
 
   return (
-    <main className="flex flex-row h-screen">
-      <div className="flex-1 flex flex-col">
-        <ModelEditor
-          className="flex-1"
-          onChange={editorChange}
-          onReset={resetModelEditorState}
-        />
-        <div className="flex flex-row p-[5px] gap-[5px]">
-          <button className="flex-2" onClick={renderPreview}>Render (F5)</button>
-          <button onClick={exportStl} className="flex-1" disabled={stlDirty}>Export STL</button>
-        </div>
-      </div>
+    <main className="h-screen">
+      <PanelGroup autoSaveId={"mainPanelGroup"} direction="horizontal" className="w-screen h-screen">
+        <Panel className="flex flex-col" defaultSize={50}>
+          <ModelEditor
+            className="flex-1"
+            onChange={editorChange}
+            onReset={resetModelEditorState}
+          />
+          <div className="flex flex-row p-[5px] gap-[5px]">
+            <button className="flex-2" onClick={renderPreview}>Render (F5)</button>
+            <button onClick={exportStl} className="flex-1" disabled={stlDirty}>Export STL</button>
+          </div>
+        </Panel>
 
-      <div className="flex-1 p-[10px] flex flex-col">
-        <div id="output-model" className="flex-1 min-h-0">
-          {/* Important: the canvas must remain mounted all the time */}
-          <RenderCanvas stl={lastStl} />
-        </div>
-        
-        <div id="output-messages" className={"font-mono text-left whitespace-break-spaces " + (stlError ? "flex-1" : "hidden")}>
-          {stlError || "Build messages will be shown here."}
-        </div>
-      </div>
+        <PanelResizeHandle />
+
+        <Panel className="flex flex-col" defaultSize={50}>
+          <div id="output-model" className="flex-1 min-h-0">
+            {/* Important: the canvas must remain mounted all the time */}
+            <RenderCanvas stl={lastStl} />
+          </div>
+          
+          <div id="output-messages" className={"font-mono text-left whitespace-break-spaces " + (stlError ? "flex-1" : "hidden")}>
+            {stlError || "Build messages will be shown here."}
+          </div>
+        </Panel>
+      </PanelGroup>
     </main>
   )
 }
